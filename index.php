e44838a9bb184d816c5b00937d823aade5d9ab27
excelee <cnexcelee@gmail.com>
2015-02-03 22:19:33 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<body>
+
+
+		<?php echo "Hello World!!!"; ?>
+		<?php 
+			$a = 50;
+			var_dump($a);
+			$b = "50";
+			var_dump($b);
+			echo "i have $a \\ \n RMB";
+
+		 ?>
+
+	</body>
+</html>
