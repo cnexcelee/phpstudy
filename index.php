7acd6dd6b7d93d9c1b60279d923a07388f79ab64
excelee <cnexcelee@gmail.com>
2015-02-03 23:09:28 +0800

    test unset() func and print() func

--- index.php
+++ index.php
@@ -2,13 +2,23 @@
 	<body>
 
 
-		<?php echo "Hello World!!!"; ?>
+		<?php echo "Hello World!!!<br>"; ?>
 		<?php 
 			$a = 50;
 			var_dump($a);
+			echo "<br>";
 			$b = "50";
 			var_dump($b);
-			echo "i have $a \\ \n RMB";
+			echo ("<br>");
+			echo "i have $a \\ \n RMB<br>";
+
+			//Test NULL
+			$t = 5;
+			echo "$t<br>";
+			unset($t);
+			// echo "$t";
+			$t = print "hello World<br>";
+			print $t
 
 		 ?>
 
