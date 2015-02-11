73eaff8a64e234844f4d895bd4658da6ebc46ac2
excelee <cnexcelee@gmail.com>
2015-02-11 22:46:59 +0800

    add input validate

--- login.php
+++ login.php
@@ -3,12 +3,33 @@
 		<title>login</title>
 	</head>
 	<body>
-	<!-- <?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?> -->
-		<form action="login.php" method="POST">
+	<?php 
+			$name = $email = $website = "";
+			if($_SERVER["REQUEST_METHOD"] == "POST"){
+				//注意 ：要判断$_POST["name"]是否为空，否则第一次进入此界面会报错，UNdefined index:name
+				$name = test_input(isset($_POST["name"]) ? $_POST["name"] : "");
+				$email = test_input(isset($_POST["email"]) ? $_POST["email"] : "");
+				$website = test_input(isset($_POST["website"]) ? $_POST["website"] :"");
+
+			}
+
+
+			function test_input($data){
+				$data = trim($data);
+				$data = stripslashes($data);
+				$data = htmlspecialchars($data);
+				return $data;
+			}
+		?>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 			Name:<input type="text" name="name">
 			E-mail:<input type="text" name="email">
 			Website:<input type="text" name="website">
 			<input type="submit" value="login">
+
+		
 		</form>
+
+		
 	</body>
 </html>
